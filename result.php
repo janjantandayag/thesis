b36fb0d4aae91997fb4cc49f60f1705f23232243
Jan-Jan <janjantandayag123@gmail.com>
2016-12-23 21:34:54 +0800

    Added function for displaying result

--- result.php
+++ result.php
@@ -1,6 +1,5 @@
 <?php
 	include('connection.php');
-
 	$emotionName = $_GET['emotion'];
 ?>
 <!DOCTYPE html>
@@ -24,152 +23,28 @@
 			</div>
 			<div class="search-content">
 				<div class="row">
+					<?php
+						$query = mysqli_query($conn, "SELECT * from emotion, emotion_food,food WHERE emotion.emotion_name LIKE '%$emotionName%' AND emotion.emotion_id = emotion_food.emotion_id AND emotion_food.food_id = food.food_id");
+    					
+    					while($row = mysqli_fetch_assoc($query)) {
+
+					?>
 					<div class="col-md-6">
 						<div class="row">
 							<div class="col-md-6">
-								<img src="img/result-1.jpg" class="img-responsive"/>
+								<img src="display-image.php?imgId=<?= $row['food_id'] ?>" width="100%" />
 							</div>
 							<div class="col-md-6">
-								<h2 class="food-header">Hamburger</h2>
-								<p class="food-description">A Hamburger (or cheeseburger when 
-								served with a slice of cheese) is a 
-								sandwich consisting of one or more 
-								cooked patties of ground meat, usually 
-								beef, placed inside a sliced bread roll</p>
-								<a href="#" class="food-button">Show more details</a>
+								<h2 class="food-header"><?= $row['food_name']; ?></h2>
+								<p class="food-description">
+									<?= $row['food_description'];?>
+								</p>
+								<a href="details.php?emotionName=<?= $emotionName ?>&id=<?=$row['food_id']?>" class="food-button">Show more details</a>
 							</div>
 						</div>
 					</div>
-                    <div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-2.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">French Fries</h2>
-								<p class="food-description">A French fries (American English), chips (British English), fries, finger chips (Indian English), or French-fried potatoes are batonnet or allumette cut deep-fried</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-				</div><div class="row">
-					<div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-1.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">Hamburger</h2>
-								<p class="food-description">A Hamburger (or cheeseburger when 
-								served with a slice of cheese) is a 
-								sandwich consisting of one or more 
-								cooked patties of ground meat, usually 
-								beef, placed inside a sliced bread roll</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-                    <div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-2.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">French Fries</h2>
-								<p class="food-description">A French fries (American English), chips (British English), fries, finger chips (Indian English), or French-fried potatoes are batonnet or allumette cut deep-fried</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-				</div><div class="row">
-					<div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-1.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">Hamburger</h2>
-								<p class="food-description">A Hamburger (or cheeseburger when 
-								served with a slice of cheese) is a 
-								sandwich consisting of one or more 
-								cooked patties of ground meat, usually 
-								beef, placed inside a sliced bread roll</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-                    <div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-2.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">French Fries</h2>
-								<p class="food-description">A French fries (American English), chips (British English), fries, finger chips (Indian English), or French-fried potatoes are batonnet or allumette cut deep-fried</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-				</div><div class="row">
-					<div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-1.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">Hamburger</h2>
-								<p class="food-description">A Hamburger (or cheeseburger when 
-								served with a slice of cheese) is a 
-								sandwich consisting of one or more 
-								cooked patties of ground meat, usually 
-								beef, placed inside a sliced bread roll</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-                    <div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-2.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">French Fries</h2>
-								<p class="food-description">A French fries (American English), chips (British English), fries, finger chips (Indian English), or French-fried potatoes are batonnet or allumette cut deep-fried</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-				</div><div class="row">
-					<div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-1.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">Hamburger</h2>
-								<p class="food-description">A Hamburger (or cheeseburger when 
-								served with a slice of cheese) is a 
-								sandwich consisting of one or more 
-								cooked patties of ground meat, usually 
-								beef, placed inside a sliced bread roll</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-                    <div class="col-md-6">
-						<div class="row">
-							<div class="col-md-6">
-								<img src="img/result-2.jpg" class="img-responsive"/>
-							</div>
-							<div class="col-md-6">
-								<h2 class="food-header">French Fries</h2>
-								<p class="food-description">A French fries (American English), chips (British English), fries, finger chips (Indian English), or French-fried potatoes are batonnet or allumette cut deep-fried</p>
-								<a href="#" class="food-button">Show more details</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+					<?php } ?>
+  				</div>
 		    <div class="pagination">
                 <div class="container">
                     <div class="row">
