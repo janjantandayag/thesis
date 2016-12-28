9c8488396132fd8479491c10dd55bd9acb12f7ef
Jan-Jan <janjantandayag123@gmail.com>
2016-12-28 11:22:15 +0800

    Added zooming effect

--- details.php
+++ details.php
@@ -143,6 +143,8 @@
 	          marker.addListener('click', function() {
 	            infoWindow.setContent(infowincontent);
 	            infoWindow.open(map, marker);
+	            map.setZoom(20);
+                map.setCenter(marker.getPosition());
 	          });
 	        });
 	      });
