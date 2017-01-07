02e17081476bb2531bee4bb8486612f391bbb9e6
Jan-Jan <janjantandayag123@gmail.com>
2017-01-07 22:25:15 +0800

    Changed UX for navigating locations

--- details.php
+++ details.php
@@ -30,17 +30,12 @@
 			</div>
 		</div>
 	</section>
-	<section id="locations">
+	<section id="locations"><!-- Trigger the modal with a button -->
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h2 class="locations--header">Location Available</h2>
+					<h2 class="locations--header">Locations Available</h2>
 				</div>
-			</div>
-		</div>
-		<div id="map-container">
-			<div class="container-fluid">
-				<div id="map"></div>
 			</div>
 		</div>
 		<div class="location-available">
@@ -91,7 +86,8 @@
 								</div>
 							</div>
 							<div class="row">
-								<a href="#map" onclick="displayMap(<?= $location['lat'] ?>, <?= $location['lang'] ?> )" class="button--showmap">Show Map</a>
+								<a href="#" data-toggle="modal" data-target="#myModal" onclick="displayMap(<?= $location['lat'] ?>, <?= $location['lang'] ?> )" class="button--showmap">Show Map</a>
+
 							</div>
 						</div>	
 					</div>
@@ -100,72 +96,90 @@
 			</div>
 		</div>
 	</section>
-
-
+	<!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+        <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">NAME OF RESTAURANT - PLACE</h4>
+                </div>
+                <div class="modal-body">
+                    <div id="map-container">
+                        <div id="map"></div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 <script>
-	function initMap(){
-	    var map = new google.maps.Map(document.getElementById('map'), {
-	      center: new google.maps.LatLng(8.9475, 125.5406),
-	      zoom: 13,
-	      styles: [
-			  {
-			    "featureType": "road.local",
-			    "elementType": "labels.text.fill",
-			    "stylers": [
-			      {
-			        "color": "#333301"
-			      }
-			    ]
-			  }
-			]
-	    });
+	// function initMap(){
+	//     var map = new google.maps.Map(document.getElementById('map'), {
+	//       center: new google.maps.LatLng(8.9475, 125.5406),
+	//       zoom: 13,
+	//       styles: [
+	// 		  {
+	// 		    "featureType": "road.local",
+	// 		    "elementType": "labels.text.fill",
+	// 		    "stylers": [
+	// 		      {
+	// 		        "color": "#333301"
+	// 		      }
+	// 		    ]
+	// 		  }
+	// 		]
+	//     });
 
-	    var infoWindow = new google.maps.InfoWindow;
-	      downloadUrl('xml.php?food_id='+<?= $foodId ?>, function(data) {
-	        var xml = data.responseXML;
-	        var markers = xml.documentElement.getElementsByTagName('marker');
-	        Array.prototype.forEach.call(markers, function(markerElem) {
-	          var name = markerElem.getAttribute('name');
-	          var point = new google.maps.LatLng(
-	              parseFloat(markerElem.getAttribute('lat')),
-	              parseFloat(markerElem.getAttribute('lng'))
-	          );
+	//     var infoWindow = new google.maps.InfoWindow;
+	//       downloadUrl('xml.php?food_id='+<?= $foodId ?>, function(data) {
+	//         var xml = data.responseXML;
+	//         var markers = xml.documentElement.getElementsByTagName('marker');
+	//         Array.prototype.forEach.call(markers, function(markerElem) {
+	//           var name = markerElem.getAttribute('name');
+	//           var point = new google.maps.LatLng(
+	//               parseFloat(markerElem.getAttribute('lat')),
+	//               parseFloat(markerElem.getAttribute('lng'))
+	//           );
 
-	          var infowincontent = document.createElement('div');
-	          var strong = document.createElement('strong');
-	          strong.textContent = name;
-	          infowincontent.appendChild(strong);
+	//           var infowincontent = document.createElement('div');
+	//           var strong = document.createElement('strong');
+	//           strong.textContent = name;
+	//           infowincontent.appendChild(strong);
 
-	          var marker = new google.maps.Marker({
-	            map: map,
-	            position: point,
-	          });
+	//           var marker = new google.maps.Marker({
+	//             map: map,
+	//             position: point,
+	//           });
 
-	          marker.addListener('click', function() {
-	            map.setZoom(20);
-                map.setCenter(marker.getPosition());
-	          });
-	        });
-	      });
-	    }
+	//           marker.addListener('click', function() {
+	//             map.setZoom(20);
+ //                map.setCenter(marker.getPosition());
+	//           });
+	//         });
+	//       });
+	//     }
 
 
-	function downloadUrl(url, callback) {
-	    var request = window.ActiveXObject ?
-	        new ActiveXObject('Microsoft.XMLHTTP') :
-	        new XMLHttpRequest;
+	// function downloadUrl(url, callback) {
+	//     var request = window.ActiveXObject ?
+	//         new ActiveXObject('Microsoft.XMLHTTP') :
+	//         new XMLHttpRequest;
 
-	    request.onreadystatechange = function() {
-	      if (request.readyState == 4) {
-	        request.onreadystatechange = doNothing;
-	        callback(request, request.status);
-	      }
-	    };
-	    request.open('GET', url, true);
-	    request.send(null);
-	 }
+	//     request.onreadystatechange = function() {
+	//       if (request.readyState == 4) {
+	//         request.onreadystatechange = doNothing;
+	//         callback(request, request.status);
+	//       }
+	//     };
+	//     request.open('GET', url, true);
+	//     request.send(null);
+	//  }
 
-  	function doNothing() {}
+ //  	function doNothing() {}
     
     function displayMap(latitude, langhitude) {
         var latitude;
@@ -177,17 +191,41 @@
 
         var map = new google.maps.Map(document.getElementById('map'), {
           center: point,
-          zoom: 16
+          zoom: 15, 
+          mapTypeControl: true,
+          mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER
+          },
+          zoomControl: true,
+          zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+          },
+          scaleControl: true,
+          streetViewControl: true,
+          streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+          },
+          fullscreenControl: true
         });
 
         var marker = new google.maps.Marker({
           map: map,
           position: point,
+          title:"The Map"
         });
+        
+        google.maps.event.addListenerOnce(map, 'idle', function () {
+		    google.maps.event.trigger(map, 'resize');
+		});
     }
 </script>
-<script async defer
+<!-- <script async defer
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9kKz7oqKE_kvxHAbuW3-32O6Uv9MHBPs&callback=initMap">
+</script> -->
+<script async defer
+src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9kKz7oqKE_kvxHAbuW3-32O6Uv9MHBPs">
 </script>
+
 </body>
 </html>
