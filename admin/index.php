3185065af5a7e1c30b04746364491741aedda86b
Jan-Jan <janjantandayag123@gmail.com>
2017-01-12 17:58:47 +0800

    Added login page

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="css/login-style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<form>
+			<input type="text" placeholder="username" required>
+			<input type="password" placeholder="password" required />
+			<input type="submit" value="Login" />
+		</form>
+	</div>
+</body>
+</html>
