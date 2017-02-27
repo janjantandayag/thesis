b7ed813e931fddc4f5b2cd1a7513d6a7bebc308b
Jan-Jan <janjantandayag123@gmail.com>
2017-02-27 17:14:11 +0800

    Added file capture testing

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="server.cgi" method="post" enctype="multipart/form-data">
+	  <tinput type="file" name="image" accept="image/*" capture="user">
+	  <input type="submit" value="Upload">
+	</form>
+</body>
+</html>
--- user/details.php
+++ user/details.php
@@ -102,7 +102,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"><?= strtoupper($location['location_name']) ?> - <?= ucwords($location['address']) ?></h4>
+                    <h4 class="modal-title" id="modal-locationName"></h4>
                 </div>
                 <div class="modal-body">
                     <div id="map-container">
@@ -181,6 +181,7 @@
  //  	function doNothing() {}
     
     function displayMap(latitude, langhitude, locationName) {
+    	document.getElementById('modal-locationName').innerHTML = locationName.toUpperCase();
         var latitude;
         var langhitude;
         var point = new google.maps.LatLng(
